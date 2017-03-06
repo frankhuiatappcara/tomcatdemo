81419d108418b6125be40dc393f1ff1eafeb2e8b
Frank Hui <frankhui@Berry.local>
2017-03-07 00:57:06 +0800

    text 11

--- hello/src/main/java/net/appcara/example/HelloServlet.java
+++ hello/src/main/java/net/appcara/example/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
                          HttpServletResponse response) throws ServletException, IOException
     {
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World 10");
+        writer.print("Hello World 11");
     }
 }
