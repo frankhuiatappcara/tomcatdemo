6c462814af7be8e6468f26121bc9d440d492d9cc
Frank Hui <frankhui@Berry.local>
2017-03-07 10:23:56 +0800

    test again and again

--- hello/src/main/java/net/appcara/example/HelloServlet.java
+++ hello/src/main/java/net/appcara/example/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
                          HttpServletResponse response) throws ServletException, IOException
     {
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World March 7 - Changed this file 12345");
+        writer.print("Hello World March 7 - Changed this file 12345 abc abc abc abc");
     }
 }
