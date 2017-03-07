c6bdd8edaf172118162490b398162dec98730962
Frank Hui <frankhui@Berry.local>
2017-03-07 10:03:13 +0800

    new changes

--- hello/src/main/java/net/appcara/example/HelloServlet.java
+++ hello/src/main/java/net/appcara/example/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
                          HttpServletResponse response) throws ServletException, IOException
     {
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World Testing abc");
+        writer.print("Hello World March 7 - 10:02");
     }
 }
