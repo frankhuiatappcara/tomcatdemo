ad4689248c5dd1431266719fa987ef681061060c
Frank Hui <frankhui@Berry.local>
2017-03-09 19:14:23 +0800

    Added JUnit test

---
+++ hello/src/test/java/net/appcara/example/Test2.java
@@ -0,0 +1,12 @@
+package net.appcara.example;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class Test2 {
+
+    @Test
+    public void testHelloWorld2() {
+        Assert.assertEquals("Hello World", "Hello World");
+    }
+}
