fb005dd7fdd659be946b5e90b73c0b51986bb30b
frankhuiatappcara <frank.hui@appcara.com>
2017-03-20 11:58:07 +0800

    Update HelloServlet.java

--- hello/src/main/java/net/appcara/example/HelloServlet.java
+++ hello/src/main/java/net/appcara/example/HelloServlet.java
@@ -65,7 +65,7 @@ public class HelloServlet extends HttpServlet {
 
          out.println("<p>==== " + count + " rows found =====</p>");
 
-         out.println("<h1>This is HA Demo!!</h1>");
+         out.println("<h1>App360 Demo - March 20</h1>");
 
          out.println("</body></html>");
       } catch (SQLException ex) {
