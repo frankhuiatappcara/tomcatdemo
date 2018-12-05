6a1082e3b5c35f9f20d91bf3e933f97d501805f9
Frank Hui <frankhui@Berry-2.local>
2018-12-05 11:23:38 +0800

    Updated text in servlet code

--- hello/src/main/java/net/appcara/example/HelloServlet.java
+++ hello/src/main/java/net/appcara/example/HelloServlet.java
@@ -65,7 +65,7 @@ public class HelloServlet extends HttpServlet {
 
          out.println("<p>==== " + count + " rows found =====</p>");
 
-         out.println("<h1>Appcara Demo - Dec 5 09:34</h1>");
+         out.println("<h1>Appcara Demo - Dec 5 11:22</h1>");
 
          out.println("</body></html>");
       } catch (SQLException ex) {
